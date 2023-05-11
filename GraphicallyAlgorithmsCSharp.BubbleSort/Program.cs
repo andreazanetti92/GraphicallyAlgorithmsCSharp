a4714784f247c2ce94db1db8fec688204169e530
Andrea Zanetti <andrea.zanetti2892@gmail.com>
2023-05-11 20:15:19 +0200

    Add project files.

---
+++ GraphicallyAlgorithmsCSharp.BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bubble Sorting Algorithm");
+
+int[] scores = { 33, 56, 20, 34, 82, 48, 95, 67, 62, 95, 88, 79, 72, 13, 9, 3, 6 };
+
+Console.WriteLine("Start sorting ...");
+sort(scores);
+
+Console.WriteLine("Sorted array: ");
+
+foreach (int score in scores)
+    Console.WriteLine(score);
+
+static void sort(int[] arrays)
+{
+
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        bool isSwap = false;
+        for (int j = 0; j < arrays.Length - 1 ; j++)
+        {
+            if (arrays[j] > arrays[j + 1])
+            {
+                // CLASSIC WAY
+                //var temp = arrays[j];
+                //arrays[j] = arrays[j + 1];
+                //arrays[j + 1] = temp;
+
+                // SWAPPING USING TUPLE
+                (arrays[j + 1], arrays[j]) = (arrays[j], arrays[j + 1]);
+                
+                isSwap = true;
+            }
+        }
+
+        if (!isSwap)
+        {
+            break;
+        }
+    }
+
+}
---
+++ GraphicallyAlgorithmsCSharp.MinValue/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Searching min value into an array of int");
+
+
+static int MinValue(int[] arrays)
+{
+    
+}
