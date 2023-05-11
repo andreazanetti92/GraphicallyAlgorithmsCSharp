ab658a5192eafc330d9713605013c3ccf68bcdfc
Andrea Zanetti <andrea.zanetti2892@gmail.com>
2023-05-11 23:16:58 +0200

    [RENAMED]

--- GraphicallyAlgorithmsCSharp.MinValue/Program.cs
+++ GraphicallyAlgorithmsCSharp.MinValue/Program.cs
@@ -1,8 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Searching min value into an array of int");
 
+int[] scores = { 33, 56, 20, 34, 82, 48, 95, 67, 62, 95, 88, 79, 72, 13, 9, 3, 6 };
+
+Console.WriteLine("Searching for min value...");
+int result = MinValue(scores);
+Console.WriteLine(result.ToString());
 
 static int MinValue(int[] arrays)
 {
-    
+    int minIndex = 0;
+	for (int j = 1; j < arrays.Length; j++)
+	{
+		if (arrays[j] < arrays[minIndex]) 
+		{ 
+			minIndex = j; 
+		}
+	}
+	return arrays[minIndex];
 }
