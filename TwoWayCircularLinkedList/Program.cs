b9e765af132c892d85a8a309e72c708949e9e705
Andrea Zanetti <andrea.zanetti2892@gmail.com>
2023-05-16 09:28:37 +0200

    TwoWayCircularLinkedList

--- TwoWayCircularLinkedList/Program.cs
+++ TwoWayCircularLinkedList/Program.cs
@@ -1,11 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic.FileIO;
+
 Console.WriteLine("Two Way Circular Linked List");
 
 Init();
+Insert(1, new Node("AB"));
+Print();
+
+Delete(1);
+Print();
 
 static void Print()
 {
+    Node p = TwoWayCircularLinkedList.head;
 
+    do
+    {
+        Console.Write($"{p.data} -> ");
+        p = p.next;
+    }
+    while (p != TwoWayCircularLinkedList.head);
+
+    Console.Write($"{p.data}\n");
+
+    p = TwoWayCircularLinkedList.tail;
+    do
+    {
+        Console.Write($"{p.data} -> ");
+        p = p.prev;
+    }while(p != TwoWayCircularLinkedList.tail);
+
+    Console.Write($"{p.data}\n\n");
+}
+
+static void Delete(int position)
+{
+    Node p = TwoWayCircularLinkedList.head;
+    int i = 0;
+
+    while(p.next != null && i < position - 1)
+    {
+        p = p.next;
+        i++;
+    }
+
+    Node temp = p.next;
+    p.next = p.next.next;
+    p.next.prev = p;
+
+}
+
+static void Insert(int position, Node node)
+{
+    Node p = TwoWayCircularLinkedList.head;
+    int i = 0;
+
+    while(p.next != null && i < position - 1)
+    {
+        p = p.next;
+        i++;
+    }
+
+    node.next = p.next;
+    p.next = node;
+    node.prev = p;
+    node.next.prev = node;
 }
 
 static void Init()
@@ -31,7 +90,7 @@ static void Init()
     TwoWayCircularLinkedList.head.prev = TwoWayCircularLinkedList.tail;
 }
 
-public class TwoWayCircularLinkedList
+public static class TwoWayCircularLinkedList
 {
     public static Node head;
     public static Node tail;
