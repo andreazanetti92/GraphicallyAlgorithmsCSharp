3dfea389c490c1a2999a0293df2f7939b32a4dea
Andrea Zanetti <andrea.zanetti2892@gmail.com>
2023-05-17 16:26:29 +0200

    Towers of Hanoi, Finbonacci and Dijkstra in progress

---
+++ Finbonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Finbonacci");
+
+Console.WriteLine("Please enter the number of month: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i + " month: " + Finbonacci(i));
+}
+
+static int Finbonacci(int n)
+{
+    if(n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Finbonacci(n - 1) + Finbonacci(n - 2);
+    }
+}
---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Towers of Hanoi");
+
+// Algorithm
+// 1. If there is only 1 disc, move it directly to C (A -> C)
+// 2. When there are 2 dics, use B as an auxiliary (A -> B, A -> C, B -> C)
+// 3. If there are more than 2 disc, use B as an auxiliary (A -> B, A -> C, B -> C) and continue to recursive process
+
+// 4. If there is only 1 disc, move it directly to C (A -> C)
+
+Console.WriteLine("Please enter the num of disc: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Hanoi(n, 'A', 'B', 'C');
+
+
+static void Hanoi(int n, char a, char b, char c)
+{
+    if(n == 1)
+    {
+        Console.WriteLine($"Move {n} {a} to {c} ");
+    }
+    else
+    {
+        Hanoi(n - 1, a, c, b);
+        Console.WriteLine($"Move {n} from {a} to {c}");
+        Hanoi(n - 1, b, a, c);
+    }
+}
